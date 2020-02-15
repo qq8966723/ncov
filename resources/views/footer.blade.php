a81961be679e65fd0c702a6690a62d06e90478f4
tux <8966723@qq.com>
2020-02-16 04:10:25 +0800

    no message

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,4 @@
-<!-- /.content-wrapper -->
-<footer class="main-footer">
+<footer>
 	<p class="text-center">全民防疫，众志成城</p>
     <p class="text-center">
     	<a href="http://sninl.com">黄冈智联</a> &copy; 鄂ICP备19030681号
