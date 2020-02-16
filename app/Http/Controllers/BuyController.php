31a7f4a89e76cec80669571031d210910ac4285e
tux <8966723@qq.com>
2020-02-16 17:04:50 +0800

    no message

--- app/Http/Controllers/BuyController.php
+++ app/Http/Controllers/BuyController.php
@@ -21,9 +21,8 @@ class BuyController extends BaseController
         $output = array(
             'url'          => $request->getPathInfo(),
             'title'        => $this->params['plot_info']['plot_name'],
+            'plot'         => $this->params['plot_info']['plot_id'],
             'goods'        => $this->params['goods_info'],
-            // 'media_source' => $this->statAdQuality->getMediaSource(),
-            // 'ad_campaign'  => $this->statAdQuality->getAdCampaign(),
         );
         return view('buy', $output);
     }
@@ -32,15 +31,15 @@ class BuyController extends BaseController
     {
         // $id     = $request->input('id');
         // $input  = $request->except(['id']);
-        // $input     = $request->input();
-        $plotInfo = $this->GetPlotInfo($plot_id);
-        if ($plotInfo != false) {
-            return redirect('/error')->with('msg', $plotInfo);
-        }
-        $goodsCate = $this->GetGoodsCate($plot_id);
-        if ($plotInfo != false) {
-            return redirect('/error')->with('msg', $plotInfo);
-        }
+        $input     = $request->input();
+        // $plotInfo = $this->GetPlotInfo($plot_id);
+        // if ($plotInfo != false) {
+        //     return redirect('/error')->with('msg', $plotInfo);
+        // }
+        // $goodsCate = $this->GetGoodsCate($plot_id);
+        // if ($plotInfo != false) {
+        //     return redirect('/error')->with('msg', $plotInfo);
+        // }
         var_dump($this->params);
     }
 }
--- resources/views/buy.blade.php
+++ resources/views/buy.blade.php
@@ -8,55 +8,57 @@
 
 @section('main')
     <h3 class="text-center">日用蔬果采购登记</h3>
-  
-    <div class="table-responsive">
-      <table class="table table-bordered">
-        <tr>
-          <td class="danger"><h4>配送信息</h4></td>
-          <td class="warning" colspan="4">
-            <input type="number" class="form-control" name="user_mobile" placeholder="手机号码">
-          </td>
-        </tr>
-        
-        <tr>
-          <td class="warning">
-            <input type="number" class="form-control" id="addr_build" placeholder="栋">
-          </td>
-          <td class="warning" colspan="2">
-            <input type="number" class="form-control" id="addr_unit" placeholder="单元">
-          </td>
-          <td class="warning" colspan="2">
-            <input type="number" class="form-control" id="addr_room" placeholder="房号">
-          </td>
-        </tr>
+    <form action="{{url('/buy/'.$plot.'/order-submit')}}">
+      @csrf
+      <div class="table-responsive">
+        <table class="table table-bordered">
+          <tr>
+            <td class="danger"><h4>配送信息</h4></td>
+            <td class="warning" colspan="4">
+              <input type="number" class="form-control" name="user_mobile" placeholder="手机号码">
+            </td>
+          </tr>
+          
+          <tr>
+            <td class="warning">
+              <input type="number" class="form-control" name="addr_build" placeholder="栋">
+            </td>
+            <td class="warning" colspan="2">
+              <input type="number" class="form-control" name="addr_unit" placeholder="单元">
+            </td>
+            <td class="warning" colspan="2">
+              <input type="number" class="form-control" name="addr_room" placeholder="房号">
+            </td>
+          </tr>
 
-        <tr>
-          <td class="danger"><h4>采购物品</h4></td>
-          <td class="danger" colspan="3"><h4>牌子</h4></td>
-          <td class="danger"><h4>数量</h4></td>
-        </tr>
-        
-        @foreach ($goods as $key => $value)
-            @for ($i = 0; $i < count($value); $i++)
-              <tr>
-                @if ($i==0)
-                  <td class="active" rowspan="{{count($value)}}">{{$key}}</td>
-                @endif
-                <td class="success" colspan="3">{{$value[$i]['goods_name']}}</td>
-                <td class="warning">
-                  <input type="number" class="form-control" id="exampleInputEmail1" placeholder="数量">
-                </td>
-              </tr>
-            @endfor
-        @endforeach
+          <tr>
+            <td class="danger"><h4>采购物品</h4></td>
+            <td class="danger" colspan="3"><h4>牌子</h4></td>
+            <td class="danger"><h4>数量</h4></td>
+          </tr>
+          
+          @foreach ($goods as $key => $value)
+              @for ($i = 0; $i < count($value); $i++)
+                <tr>
+                  @if ($i==0)
+                    <td class="active" rowspan="{{count($value)}}">{{$key}}</td>
+                  @endif
+                  <td class="success" colspan="3">{{$value[$i]['goods_name']}}</td>
+                  <td class="warning">
+                    <input type="number" class="form-control" name="{{'goods_id'.$value[$i]['goods_id']}}" placeholder="数量">
+                  </td>
+                </tr>
+              @endfor
+          @endforeach
 
-        <tr>
-          <td class="active" colspan="5">
-            <button type="button" class="btn btn-primary btn-lg btn-block">提交</button>
-          </td>
-        </tr>
-      </table>
-    </div>
+          <tr>
+            <td class="active" colspan="5">
+              <button type="submit" class="btn btn-primary btn-lg btn-block">提交</button>
+            </td>
+          </tr>
+        </table>
+      </div>
+    </form>
 
 @endsection
 
