88774dd2c09805a8209212a3bd3da9e2117c000a
tux <8966723@qq.com>
2020-02-16 16:13:32 +0800

    no message

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,7 +2,7 @@
 	<hr/>
     <p class="text-center">
     	<a href="login.html">社区管理员入口</a>  <a href="contact.html">其他社区使用申请</a>
-    	<br/>
+    	<br/><br/>
     	<a href="http://sninl.com">黄冈智联网络公司</a> &copy;鄂ICP备19030681号
     </p>
 </footer>
